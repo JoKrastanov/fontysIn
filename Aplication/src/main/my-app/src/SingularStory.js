1eb7d3a3f35f55fff06d71f90d63685b6a05cf1d
Julius <zughiv@gmail.com>
2021-11-03 14:18:52 +0100

    Css and element split
    Partially done css and element splitting for the sprint 2 presentation

---
+++ Aplication/src/main/my-app/src/SingularStory.js
@@ -0,0 +1,27 @@
+import React, {useEffect, useState} from "react";
+import {getProjectsFromAccount, getAccountData,getAccount} from "./services";
+export default function SingularStory (prop){
+        if(prop.story != undefined){
+            return (
+                <div id="Story2">
+                    {console.log(prop)}
+                    <svg className="Rectangle_5">
+                        <rect id="Rectangle_5" rx="30" ry="30" x="0" y="0" width="1042" height="270">
+                        </rect>
+                    </svg>
+                    <div id="n_0_Sep">
+                        <span>10Sep</span>
+                    </div>
+                    <div id="Story_to_end_all_stories">
+                        <span>{prop.story.title}</span>
+                    </div>
+                    <div id="Phasellus_id_risus_facilisis_f">
+                        <span>{prop.story.description}</span><br/><span>Click for more...</span>
+                    </div>
+                </div>
+            )
+        }
+        else return <></>
+
+
+}
