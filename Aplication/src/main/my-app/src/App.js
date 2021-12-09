adf10db70f3a9c3a792a0754e530f4b333d04f92
Chernaev,Angel A.V <a.chernaev@student.fontys.nl>
2021-12-09 14:44:44 +0100

    portfolio exporter implemented

--- Aplication/src/main/my-app/src/App.js
+++ Aplication/src/main/my-app/src/App.js
@@ -1,11 +1,15 @@
 import React, { Component, useState, useEffect } from 'react';
 import './App.css';
+import ReactDOM from 'react-dom';
 import TopBar from "./TopBar";
 import AccountPage from "./AccountPage";
 import { getAccount } from "./services";
 import Login from './Login';
 import MessageIcon from "./MessageIcon";
 import LastChats from "./LastChats";
+import ProjectsExporter from './Portfolio/ProjectsExporter';
+import { PDFViewer } from '@react-pdf/renderer';
+import Popup from './components/Popup';
 
 function App() {
     const [pcn, setPcn] = useState();
@@ -15,6 +19,11 @@ function App() {
     const rendered = { hasRendered, setHasRendered, hasRendered2, setHasRendered2, hasRendered3, setHasRendered3 }
     const [myAccount, setMyAccount] = useState(true);
     const [openedChat, setOpenedChat] = useState(0);
+    const [buttonPopupPdf, setButtonPopupPdf] = useState(false);
+
+    function updateButton (){
+       setButtonPopupPdf(true);
+    }
 
     function setPcnStates(pcn) {
         setPcn(pcn);
@@ -29,9 +38,14 @@ function App() {
             <>
                 <div><TopBar setPcn={setPcn} setPcnStates={setPcnStates} setMyAccount={setMyAccount} /></div>
                 <div>
+                <button onClick={updateButton} className='openPDF'>Click here to export to PDF</button>
                     <AccountPage pcn={pcn} setPcn={setPcn} rendered={rendered} setPcnStates={setPcnStates} myAccount={myAccount} />
-                </div>
+                    <Popup trigger={buttonPopupPdf} setTrigger={setButtonPopupPdf}>
+                    <PDFViewer><ProjectsExporter/></PDFViewer>
+                    </Popup>                    
+               </div>
                 <div>
+                   
 
                 </div>
                 <div id={"messages"}><MessageIcon openChat={setOpenedChat} openedChat={openedChat} pcn={pcn}/></div>
