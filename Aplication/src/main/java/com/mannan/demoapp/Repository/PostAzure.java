652aedcd9ae926d71ed8c5db9d0a619c723de332
Joan <jokrastanov@gmail.com>
2022-01-17 16:13:17 +0100

    Posts include Name and Image

--- Aplication/src/main/java/com/mannan/demoapp/Repository/PostAzure.java
+++ Aplication/src/main/java/com/mannan/demoapp/Repository/PostAzure.java
@@ -42,12 +42,12 @@ public class PostAzure implements IPostAzure {
     public Post getPostById(Long id) throws SQLException {
         Connection connection = DriverManager.getConnection(con.getCon());
         try {
-            PreparedStatement selectSql = connection.prepareStatement("Select * from Post " +
-                    "where id = ?");
+            PreparedStatement selectSql = connection.prepareStatement("Select p.id, a.Name, a.[Image],  p.Title, CAST(p.[Description] as varchar(max)), p.[Date], p.AccountPCN from Post as p inner join account as a on a.PCN = p.AccountPCN" +
+                    " where p.id = ?");
             selectSql.setLong(1, id);
             ResultSet result = selectSql.executeQuery();
             while(result.next()) {
-                return new Post(result.getLong(1), result.getString(2), result.getString(3), result.getLong(4), result.getString(5));
+                return new Post(result.getLong(1), result.getString(2), result.getString(3), result.getString(4), result.getString(5), result.getLong(7), result.getString(6) );
             }
         }
         catch (SQLException e) {e.printStackTrace();}
@@ -60,13 +60,13 @@ public class PostAzure implements IPostAzure {
         Connection connection = DriverManager.getConnection(con.getCon());
         List<Post> posts = new ArrayList<>();
         try {
-            PreparedStatement selectSql = connection.prepareStatement("Select * from Post " +
-                    "Where AccountPcn = ?");
+            PreparedStatement selectSql = connection.prepareStatement("Select p.id, a.Name, a.[Image],  p.Title, CAST(p.[Description] as varchar(max)), p.[Date], p.AccountPCN from Post as p inner join account as a on a.PCN = p.AccountPCN" +
+                    " Where AccountPcn = ?");
             selectSql.setLong(1, pcn);
             ResultSet result = selectSql.executeQuery();
 
             while(result.next()) {
-                posts.add(new Post(result.getLong(1), result.getString(2), result.getString(3), result.getLong(4), result.getString(5)));
+                posts.add(new Post(result.getLong(1), result.getString(2), result.getString(3), result.getString(4), result.getString(5), result.getLong(7), result.getString(6)));
             }
             return posts;
         }
@@ -96,13 +96,12 @@ public class PostAzure implements IPostAzure {
         Connection connection = DriverManager.getConnection(con.getCon());
         try {
             List<Post> posts = new ArrayList<>();
-           PreparedStatement selectSql = connection.prepareStatement("select distinct p.id, p.Title, CAST(p.[Description] as varchar(max)), p.[Date], p.AccountPCN from post as p" +
-                   " inner join Connection as c on p.AccountPCN = c.Pcn1 or p.AccountPCN = c.Pcn2 where c.pcn1 = ? or c.pcn2 = ? ORDER by id DESC");
+           PreparedStatement selectSql = connection.prepareStatement("select distinct p.id, a.Name, a.[Image],  p.Title, CAST(p.[Description] as varchar(max)), p.[Date], p.AccountPCN from post as p inner join account as a on a.PCN = p.AccountPCN inner join Connection as c on p.AccountPCN = c.Pcn1 or p.AccountPCN = c.Pcn2  where c.pcn1 = ? or c.pcn2 = ? ORDER by id DESC");
            selectSql.setLong(1, pcn);
            selectSql.setLong(2, pcn);
-           ResultSet resultSet = selectSql.executeQuery();
-           while (resultSet.next()) {
-               posts.add(new Post(resultSet.getLong(1), resultSet.getString(2),resultSet.getString(3), resultSet.getLong(5), resultSet.getString(4)));
+           ResultSet result = selectSql.executeQuery();
+           while (result.next()) {
+               posts.add(new Post(result.getLong(1), result.getString(2), result.getString(3), result.getString(4), result.getString(5), result.getLong(7), result.getString(6)));
            }
            return posts;
         }
